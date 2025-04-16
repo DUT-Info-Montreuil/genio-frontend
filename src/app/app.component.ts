dedda88539f56b66715bda53b86a8b7a83769dcb
elsouche123 <elsa.simha.hadjadj@gmail.com>
2025-04-16 12:48:17 +0200

    Commencement du site

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FooterComponent} from './shared/footer/footer.component';
+import {InscriptionComponent} from './pages/inscription/inscription.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent, InscriptionComponent],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AccueilComponent } from './pages/accueil/accueil.component';
+import { InscriptionComponent } from './pages/inscription/inscription.component';
+import {ConnexionComponent} from './pages/connexion/connexion.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: AccueilComponent },
+  { path: 'inscription', component: InscriptionComponent },
+  { path: 'connexion', component: ConnexionComponent }
+];
