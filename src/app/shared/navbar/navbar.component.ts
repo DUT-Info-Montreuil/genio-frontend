dca371941943eeeaaeefa49e01109f6dbc5a70a4
elsouche123 <elsa.simha.hadjadj@gmail.com>
2025-05-13 14:44:14 +0200

    footer et navbar ux/ui e't wcag tous est bon

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,20 +1,23 @@
 import { Component } from '@angular/core';
-import { Router, RouterLink } from '@angular/router';
-import { NgIf } from '@angular/common';
+import {Router, RouterLink} from '@angular/router';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  standalone: true,
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
-  imports: [RouterLink, NgIf]
+  standalone: true,
+  imports: [
+    RouterLink,
+    NgIf
+  ]
 })
 export class NavbarComponent {
-  currentRoute: string = '';
   navbarOpen = false;
+  currentRoute = '';
 
   constructor(private router: Router) {
-    router.events.subscribe(() => {
+    this.router.events.subscribe(() => {
       this.currentRoute = this.router.url;
       this.navbarOpen = false;
     });
@@ -31,7 +34,7 @@ export class NavbarComponent {
       '/donnees-personnelles',
       '/plan-du-site',
       '/reset-password',
-      '/mot-de-passe-oublie'
+      '/mot-de-passe-oublie',
     ].includes(this.currentRoute);
   }
 
