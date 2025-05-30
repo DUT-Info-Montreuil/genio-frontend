d54cbf2342281649588d0c70157661bc1a60d785
elsouche123 <elsa.simha.hadjadj@gmail.com>
2025-05-30 14:35:05 +0200

    ajout du copyright dans le front

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,3 +1,17 @@
+/**
+ *  GenioService Frontend
+ *  ---------------------
+ *  Copyright (c) 2025
+ *  Elsa HADJADJ <elsa.simha.hadjadj@gmail.com>
+ *
+ *  Licence sous Creative Commons CC-BY-NC-SA 4.0.
+ *  Vous pouvez consulter la licence ici :
+ *  https://creativecommons.org/licenses/by-nc-sa/4.0/
+ *
+ *  Dépôt GitHub (Frontend) :
+ *  https://github.com/DUT-Info-Montreuil/genio-frontend
+ */
+
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
